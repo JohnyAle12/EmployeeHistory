a4d5e2c294993353f9d6d6515f60957d26b371c9
Johny Prieto <johnyprieto001@gmail.com>
2023-02-10 11:10:33 -0500

    get history response

--- src/application/controllers/historico-controller.ts
+++ src/application/controllers/historico-controller.ts
@@ -1,13 +1,25 @@
-import { IHistoricoApi, IResponseApi } from "../interfaces/response-controller-interface"
+import { StatusCodes } from "http-status-codes";
+import HistoricoUseCase from "src/domain/usecase/historico-usecase";
+import { responseFail, responseSuccess } from "../helpers/response";
+import { IHistoricoApi, IResponseApi, IHistoricoMethod } from '../interfaces/response-controller-interface';
 
 class HistoricoController implements IHistoricoApi {
 
+    private historyUseCase: IHistoricoMethod;
+
     constructor(){
+        this.historyUseCase = new HistoricoUseCase();
     }
 
     getHistoricoEmpleado = async (empleadoId: number) => {
-
         let response: IResponseApi;
+
+        try {
+            const result = await this.historyUseCase.getHistoricoEmpleado(empleadoId)
+            response = responseSuccess({ data:result }, StatusCodes.OK);
+        } catch (error) {
+            response = responseFail(error);   
+        }
 
         return response;
     
--- src/infraestruture/handlers/functions/getHistoricoEmpleado/handler.ts
+++ src/infraestruture/handlers/functions/getHistoricoEmpleado/handler.ts
@@ -4,17 +4,13 @@ import type { ValidatedEventAPIGatewayProxyEvent } from '../../libs/apiGateway';
 import { formatJSONResponse } from '../../libs/apiGateway';
 import { middyfy } from '../../libs/lambda';
 import { IResponseApi } from 'src/application/interfaces/response-controller-interface';
+import HistoricoController from '../../../../application/controllers/historico-controller';
+
+const historyController = new HistoricoController();
 
 const getHistoricoEmpleado: ValidatedEventAPIGatewayProxyEvent<any> = async (event) => {
 
-  const response: IResponseApi = {
-    statusCode: 200,
-    success: true,
-    data: {
-      empleadoId: + event.pathParameters.empleadoId
-    },
-    message: "message test"
-  }
+  const response: IResponseApi = await historyController.getHistoricoEmpleado(+event.pathParameters.empleadoId);
   return formatJSONResponse(response.statusCode, response);
 }
 
